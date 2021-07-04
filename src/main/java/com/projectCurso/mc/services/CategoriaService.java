7f8674762ad47b163fd3174d419a8b1d32eb256d
Bruno Santos <brunocruz55171@gmail.com>
2021-07-04 17:50:02 -0300

    Ajustes no endPoint /categorias/{id}

--- src/main/java/com/projectCurso/mc/services/CategoriaService.java
+++ src/main/java/com/projectCurso/mc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.projectCurso.mc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projectCurso.mc.domain.Categoria;
 import com.projectCurso.mc.repositories.CategoriaRepository;
+import com.projectCurso.mc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +16,11 @@ public class CategoriaService {
 	@Autowired // automaticamente será instanciado pelo Spring, mecanismo de injeção de dependência
 	private CategoriaRepository repo;
 
-	public Categoria find(Integer id) {
+	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+										"Objeto não encontrado! Id: "+ id + ", Tipo: "+ Categoria.class.getName()));
 
 	}
 
