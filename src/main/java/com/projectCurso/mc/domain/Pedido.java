f9ec3908d958cba25c8b908cd7ffe5f5b7c146e8
Bruno Santos <brunocruz55171@gmail.com>
2021-07-12 20:13:04 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/projectCurso/mc/domain/Pedido.java
+++ src/main/java/com/projectCurso/mc/domain/Pedido.java
@@ -1,7 +1,11 @@
 package com.projectCurso.mc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -32,7 +37,10 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "enderecoDeEntrega_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy="id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
+	public Pedido() {}
 	
 	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
@@ -42,7 +50,14 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
-	public Pedido() {}
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for(ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+		
+	}
 
 	public Integer getId() {
 		return id;
@@ -83,6 +98,14 @@ public class Pedido implements Serializable {
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -108,6 +131,8 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
+
 	
 	
 }
---
+++ src/main/java/com/projectCurso/mc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,16 @@
+package com.projectCurso.mc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projectCurso.mc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+	
+	
+	
+	
+}
